970b4d697cbc8042fe982c33f3552f67d62b5e75
CeoOfStoicism <nikola.lukov.2006@gmail.com>
2023-06-03 16:36:58 +0300

    Fixing

--- src/handlers/indexHandlers.js
+++
@@ -1,5 +0,0 @@
-const homeHandler = require('./home');
-const staticHandler = require('./static-files');
-const catsHandler = require('./cat');
-
-module.exports = [homeHandler, staticHandler, catsHandler];
